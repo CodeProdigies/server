05cdd5164751322d7979f14a159c736077b211fc
melrpm <medelorbe@outlook.com>
2024-05-25 08:08:28 -0400

    comments

--- Services/DocumentsService.cs
+++ Services/DocumentsService.cs
@@ -1,5 +1,4 @@
 ﻿using prod_server.Entities;
-using System.Reflection.Metadata;
 using iText.Kernel.Pdf;
 using iText.Forms;
 
@@ -17,18 +16,20 @@ namespace prod_server.Services
         public async Task<byte[]> CreateQuote(Quote quote)
         {
 
-            using (var stream = new MemoryStream())
+            using (var stream = new MemoryStream()) // We keep the file in memory inside stream.
             {
+                // open the file and set it to the stream.
                 var pdf = new PdfDocument(new PdfReader("./Assets/Templates/ExampleDoc.pdf"), new PdfWriter(stream));
 
                 // Access the form fields
                 PdfAcroForm form = PdfAcroForm.GetAcroForm(pdf, true);
                 form.GetField("date").SetValue(DateTime.UtcNow.ToString("dd/MM/YYYY"));
+                
                 // lock pdf; Not editable.
-
                 form.FlattenFields();
                 pdf.Close();
 
+                // Convert to byte[] (I think is most generic data type).
                 return stream.ToArray();
             }
 
